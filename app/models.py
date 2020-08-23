a22a7db33a91ce69450ce73d5ae1bf7e850349a3
rj425 <rj81309050@gmail.com>
2020-08-23 16:30:59 +0530

    Added Task api structure

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, to_field='username')
+    url = models.URLField(max_length=200, null=False, blank=False)
+    movieCount = models.PositiveIntegerField(default=0)
+    createdDate = models.DateTimeField(auto_now_add=True)
+    modifiedDate = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Task-{}".format(self.id)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from app.models import Task
+from app.serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows tasks to be created.
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        serializer = TaskSerializer(data=data)
+        if serializer.is_valid():
+            validatedData = serializer.validated_data
+            taskInstance = Task.objects.create(**validatedData)
+            # Scrape movies
+            
+            return Response(TaskSerializer(taskInstance).data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
